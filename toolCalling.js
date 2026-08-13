312339e7d36e2f3ba4465c7ff55ae8e13388fb57
ApuKM <apukumar180@gmail.com>
2026-08-14 02:02:12 +0600

    Implementing web search tool using tavily and log dynamic data in the console

--- toolCalling.js
+++ toolCalling.js
@@ -5,21 +5,23 @@ const groq = new Groq();
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 async function main() {
+  const messages = [
+    {
+      role: "system",
+      content: `You're Guddu, a smart personal assistant who answers the asked questions.
+           You have access to the following tools:
+           1.get_weather({location}: {location: string})`,
+    },
+    {
+      role: "user",
+      content: "What is the current weather in Rajshahi?",
+    },
+  ];
+
   const completion = await groq.chat.completions.create({
     model: "llama-3.3-70b-versatile",
     temperature: 0,
-    messages: [
-      {
-        role: "system",
-        content: `You're Guddu, a smart personal assistant who answers the asked questions.
-           You have access to the following tools:
-           1.get_weather({location}: {location: string})`,
-      },
-      {
-        role: "user",
-        content: "What is the current weather in Dhaka?",
-      },
-    ],
+    messages: messages,
     tools: [
       {
         type: "function",
@@ -47,6 +49,8 @@ async function main() {
     tool_choice: "auto",
   });
 
+  messages.push(completion.choices[0].message);
+
   const toolCalls = completion.choices[0]?.message?.tool_calls;
   if (!toolCalls) {
     console.log(`Assistant: ${completion.choices[0]?.message.content}`);
@@ -60,10 +64,23 @@ async function main() {
 
     if (functionName === "get_weather") {
       const toolResult = await get_weather(JSON.parse(functionParams));
-      console.log("toolResult:", toolResult);
+    //   console.log("toolResult:", toolResult);
+      messages.push({
+        tool_call_id: tool.id,
+        role: "tool",
+        name: functionName,
+        content: toolResult,
+      });
     }
   }
-  //   console.log(completion.choices[0]?.message);
+
+  //calling llm with the tool result
+  const completion2 = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    temperature: 0,
+    messages: messages,
+  });
+  console.log(completion2.choices[0].message.content);
 }
 
 main().catch(console.error);
@@ -71,11 +88,11 @@ main().catch(console.error);
 //get_weather tool(function)
 async function get_weather({ location }) {
   console.log("get_weather function calling...");
-  const response = await tvly.search(location);
-//   console.log("Response", response);
+  const response = await tvly.search(`current weather in ${location}`);
+  //   console.log("Response", response);
   const finalResult = response.results
     .map((result) => result.content)
     .join("\n\n");
-//   console.log("FinalResult:", finalResult);
+  //   console.log("FinalResult:", finalResult);
   return finalResult;
 }
