bd2484b22c1a7f4d56f022344df2afc77cff5d4d
ApuKM <apukumar180@gmail.com>
2026-08-13 19:12:51 +0600

    implementing streaming in the output

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ import Groq from "groq-sdk";
 
 const groq = new Groq();
 async function main() {
-  const completion = await groq.chat.completions.create({
+  const stream = await groq.chat.completions.create({
     temperature: 0.2,
     // top_p: 1,
-    stop: "today",
     max_completion_tokens: 1000,
+    stop: "today",
     // frequency_penalty: 1,
     // presence_penalty: 1,
+    stream: true,
     model: "llama-3.3-70b-versatile",
     messages: [
       {
@@ -21,7 +22,11 @@ async function main() {
       },
     ],
   });
-  console.log(completion.choices[0]?.message?.content);
+  // Loop through the stream chunks as they arrive
+  for await (const chunk of stream) {
+    process.stdout.write(chunk.choices[0]?.delta?.content || "");
+  }
+ 
 }
 
 main().catch(console.error);
