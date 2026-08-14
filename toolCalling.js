06adf440ca300c6e27d9904a9c0b03339cfdc08d
ApuKM <apukumar180@gmail.com>
2026-08-14 15:56:16 +0600

    [Added]: while loops for multiple tool call & to get user questions from cmd line interface which is created using node readline interface

--- toolCalling.js
+++ toolCalling.js
@@ -1,3 +1,4 @@
+import readline from "node:readline/promises";
 import Groq from "groq-sdk";
 import { tavily } from "@tavily/core";
 
@@ -5,74 +6,93 @@ const groq = new Groq();
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 async function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
   const messages = [
     {
       role: "system",
       content: `You're Guddu, a smart personal assistant who answers the asked questions.
-           You have access to the following tools:
-           1.webSearch({query}: {query: string})`,
+      You have access to the following tools:
+      1.webSearch({query}: {query: string}).
+      current date and time: ${new Date().toUTCString()}
+      `,
     },
-    {
-      role: "user",
-      content: "hen was I phone 17 was launched?",
-      //What is the current weather in Rajshahi?
-      //When was I phone 17 was launched?
-    },
+    // {
+    //   role: "user",
+    //   content: "When was I phone 17 was launched?",
+    //   //What is the current weather in Rajshahi?
+    //   //When was I phone 17 was launched?
+    // },
   ];
 
   while (true) {
-    const completion = await groq.chat.completions.create({
-      model: "llama-3.3-70b-versatile",
-      temperature: 0,
-      messages: messages,
-      tools: [
-        {
-          type: "function",
-          function: {
-            name: "webSearch",
-            description: "Get the latest data for searched query",
-            parameters: {
-              // JSON Schema object
-              type: "object",
-              properties: {
-                query: {
-                  type: "string",
-                  description: "User searched query",
-                },
-              },
-              required: ["query"],
-            },
-          },
-        },
-      ],
-      tool_choice: "auto",
-    });
-
-    messages.push(completion.choices[0].message);
-
-    const toolCalls = completion.choices[0]?.message?.tool_calls;
-    if (!toolCalls) {
-      console.log(`Assistant: ${completion.choices[0]?.message.content}`);
+    const question = await rl.question("You: ");
+    // console.log("question", question)
+    if (question === "bye") {
       break;
     }
+    messages.push({
+      role: "user",
+      content: question,
+    });
+    while (true) {
+      const completion = await groq.chat.completions.create({
+        model: "llama-3.3-70b-versatile",
+        temperature: 0,
+        messages: messages,
+        tools: [
+          {
+            type: "function",
+            function: {
+              name: "webSearch",
+              description:
+                "Get the latest and realtime data for searched query",
+              parameters: {
+                // JSON Schema object
+                type: "object",
+                properties: {
+                  query: {
+                    type: "string",
+                    description: "User searched query",
+                  },
+                },
+                required: ["query"],
+              },
+            },
+          },
+        ],
+        tool_choice: "auto",
+      });
 
-    for (const tool of toolCalls) {
-      // console.log("tool", tool);
-      const functionName = tool.function.name;
-      const functionParams = tool.function.arguments;
+      messages.push(completion.choices[0].message);
 
-      if (functionName === "webSearch") {
-        const toolResult = await webSearch(JSON.parse(functionParams));
-        //   console.log("toolResult:", toolResult);
-        messages.push({
-          tool_call_id: tool.id,
-          role: "tool",
-          name: functionName,
-          content: toolResult,
-        });
+      const toolCalls = completion.choices[0]?.message?.tool_calls;
+      if (!toolCalls) {
+        console.log(`Assistant: ${completion.choices[0]?.message.content}`);
+        break;
+      }
+
+      for (const tool of toolCalls) {
+        // console.log("tool", tool);
+        const functionName = tool.function.name;
+        const functionParams = tool.function.arguments;
+
+        if (functionName === "webSearch") {
+          const toolResult = await webSearch(JSON.parse(functionParams));
+          //   console.log("toolResult:", toolResult);
+          messages.push({
+            tool_call_id: tool.id,
+            role: "tool",
+            name: functionName,
+            content: toolResult,
+          });
+        }
       }
     }
   }
+  rl.close();
 }
 
 main().catch(console.error);
